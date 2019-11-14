5d9f2c4a003ce200d73fbabc7fde3961c0997a49
prelex <56546267+prelex@users.noreply.github.com>
2019-11-14 13:40:56 +0000

    rename vertices to cubeVertices

--- LearnOpenGL-lighting1/Main.cpp
+++ LearnOpenGL-lighting1/Main.cpp
@@ -75,7 +75,7 @@ int main()
 
 	// cube vertices
 	// defined in the appropriate order for face culling
-	float vertices[] = {
+	float cubeVertices[] = {
 	// position           // normals		   // texture coords
 	// back face
 	-0.5f, -0.5f, -0.5f,  0.0f,  0.0f, -1.0f,  0.0f, 0.0f, // bottom-right
@@ -145,7 +145,7 @@ int main()
 	glGenVertexArrays(1, &cubeVAO);
 	glGenBuffers(1, &VBO);
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(cubeVertices), cubeVertices, GL_STATIC_DRAW);
 	glBindVertexArray(cubeVAO);
 
 	// position attribute
