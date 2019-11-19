1079ad1388c7a28cc0d388cf3b4c3ae8826c5b81
prelex <56546267+prelex@users.noreply.github.com>
2019-11-19 15:08:48 +0000

    enable msaa

--- LearnOpenGL-lighting1/Main.cpp
+++ LearnOpenGL-lighting1/Main.cpp
@@ -45,6 +45,7 @@ int main()
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	glfwWindowHint(GLFW_SAMPLES, 4);
 
 	// glfw: create window
 	GLFWwindow* window = glfwCreateWindow(screenWidth, screenHeight, "LearnOpenGL", NULL, NULL);
@@ -250,6 +251,7 @@ int main()
 
 	glEnable(GL_DEPTH_TEST);
 	glEnable(GL_CULL_FACE);
+	glEnable(GL_MULTISAMPLE);
 
 	// load textures
 	unsigned int cubeDiffuseMap = loadTexture("images/container2.png", true);
